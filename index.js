df86c5e902f8f9ed6b79d6d9d41f3eb72b63c379
aakashtripathi306 <aakash.tripathi.mca@ghrua.edu.in>
2025-06-28 20:27:53 +0530

    fix

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/",(req,res)=>{
-    res.send("ecipe-Backend Running")
+    res.send("Recipe-Backend Running")
 })
 
 app.use('/api/auth',authRoutes)
